76fe02b24ae07f777d5e273a0642b5e36e3c1b01
jgumerove <josh.gumerove@gmail.com>
2021-05-20 17:51:29 -0400

    added restful actions for CRUD

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,9 +5,25 @@ class CardsController < ApplicationController
         erb :"cards/index"
     end
 
+    get "/cards/new" do
+        erb :"cards/new"
+    end
+
     get "/cards/:id" do
         @card = Card.find(params[:id])
         erb :"cards/show"
-      end
+    end
+
+    post "/cards" do
+        card = current_user.cards.create(params[:card])
+        redirect "/cards/#{card.id}"
+    end
+
+    get "/cards/:id/edit" do
+        @card = Card.find(params[:id])
+        erb :"cards/edit"
+    end
+
+    
 
 end

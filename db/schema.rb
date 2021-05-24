f0c8f6db798e1bab6f10d2bc0e67fc5952436d2a
jgumerove <josh.gumerove@gmail.com>
2021-05-24 19:08:58 -0400

    add image to cards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_20_181708) do
+ActiveRecord::Schema.define(version: 2021_05_24_224602) do
 
   create_table "cards", force: :cascade do |t|
     t.string "player_name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_05_20_181708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|

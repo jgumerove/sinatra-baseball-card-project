46ba88bd4703ff2b73d902715d984e2e20eb4149
jgumerove <josh.gumerove@gmail.com>
2021-06-01 15:34:22 -0400

    add validation to prevent blank username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password 
     has_many :cards
+    validates :username, presence: true
     validates :username, uniqueness: true 
     validates :password, length: { in: 6..20 }
     

42baafc455b480341fee3bd24a7281874a5cefe5
jgumerove <josh.gumerove@gmail.com>
2021-06-01 15:06:29 -0400

    add flash message to login if incorrect info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id
             redirect "/" 
         else
+            flash[:message] = "Incorrect username or password"
             redirect "/login"
         end
     end

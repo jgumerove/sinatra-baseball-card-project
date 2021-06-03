22a332be0ed02a13e024f875f55acad9b6a9f079
jgumerove <josh.gumerove@gmail.com>
2021-06-03 14:56:34 -0400

    place separate comma method in model folder for card and refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,28 +29,6 @@ class ApplicationController < Sinatra::Base
       redirect "/login" if !current_user
     end
 
-    def separate_comma(number)
-      a = number.to_s.split('')
-      b = a.size/3.0
-      if a.size < 4
-         number.to_s 
-      elsif a.size%3 == 0
-        n = -4
-        (b.to_i-1).times do |i|
-          a.insert(n, ',')
-          n -= 4
-        end
-         a.join("")
-      else
-          n = -4
-        b.to_i.times do |i|
-          a.insert(n, ',')
-          n -= 4
-        end
-        a.join("")
-      end
-    end
-
   end
 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -3,5 +3,27 @@ class Card < ActiveRecord::Base
     validates :player_name, :brand, :year, presence: :true
     validates :price, numericality: {greater_than_or_equal_to: 0}
     validates :year, numericality: {greater_than_or_equal_to: 1850, less_than_or_equal_to: Date.today.year}
+
+    def separate_comma
+        a = self.price.to_s.split('')
+        b = a.size/3.0
+        if a.size < 4
+           self.price.to_s 
+        elsif a.size%3 == 0
+          n = -4
+          (b.to_i-1).times do |i|
+            a.insert(n, ',')
+            n -= 4
+          end
+           a.join("")
+        else
+            n = -4
+          b.to_i.times do |i|
+            a.insert(n, ',')
+            n -= 4
+          end
+          a.join("")
+        end
+      end
     
 end

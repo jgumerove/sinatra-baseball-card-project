8319434930227ba38c4ed78f7535a31f02af774d
jgumerove <josh.gumerove@gmail.com>
2021-06-03 22:50:35 -0400

    fix views for updated separate comma method

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
     validates :username, presence: true
     validates :username, uniqueness: true 
     validates :password, length: { in: 6..20 }
+
+    
     
 end
